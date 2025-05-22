ddab5159691f8f9d7021368387874b86f879e88d
Arman Hossain Akash <armanhossainakash52@gmail.com>
2025-05-22 19:58:09 +0600

    first commit

---
+++ mil5mod25.js
@@ -0,0 +1,8 @@
+const h1Cach=document.getElementsByTagName('h1');
+console.log(h1Cach);
+
+
+const sectionArea = document.querySelectorAll('section');
+for( const sectionItmes of sectionArea){
+  console.log(sectionItmes);
+}
