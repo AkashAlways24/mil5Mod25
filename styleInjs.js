14981f6cca940beedb0fccd9d8aef0017a82e7ee
Arman Hossain Akash <armanhossainakash52@gmail.com>
2025-05-23 12:12:01 +0600

    add stylejs.js

---
+++ styleInjs.js
@@ -0,0 +1,7 @@
+// const pSec = document.getElementsByClassName("pClass");
+// pSec.classList.add('colorMix');
+
+const newClass = document.getElementsByClassName('pClass');
+newClass.classList.remove('colorMix');
+// console.log(newClass);
+
